731634ef8acdc02cbb47507258eadcfa561fffa4
Vyshnavi <moosinivyshnavi222@gmail.com>
2026-06-01 21:30:39 +0530

    feat(product-service): add Product entity, repository, service, controller

---
+++ product-service/src/main/java/com/smartshop/product_service/ProductServiceApplication.java
@@ -0,0 +1,15 @@
+package com.smartshop.product_service;
+
+import org.springframework.boot.SpringApplication;
+
+public class ProductServiceApplication {
+
+	public static void main(String[] args) {
+		
+			SpringApplication.run(ProductServiceApplication.class, args);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
